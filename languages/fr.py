7e198794714169382b33299a7b278f5d7487d4af
Nolan Pauzat <npauzat@sio-coliniere44.local>
2016-04-19 16:07:16 +0200

    Traduction message A1

--- languages/fr.py
+++ languages/fr.py
@@ -99,7 +99,7 @@
 'Informations sur la M2L': 'Informations sur la M2L',
 'insert new': 'insérer un nouveau',
 'insert new %s': 'insérer un nouveau %s',
-'Insufficient privileges': 'Insufficient privileges',
+'Insufficient privileges': 'Droits insuffisants',
 'Internal State': 'État interne',
 'Intranet': 'Intranet',
 'Introduction': 'Introduction',
